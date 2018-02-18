be5d828be9f6d425f4467ddd29e994a69439bf51
sheafitzek <agg817@sbcglobal.net>
2018-02-18 13:01:53 -0600

    project base

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App';
+
+render(<App />, document.getElementById(`root`));
+
+registerServiceWorker();

6755c01db2c4737b447e01d75d4c8c5ac814e2ca
sheafitzek <agg817@sbcglobal.net>
2018-02-18 16:46:03 -0600

    send payload to redux-promise and then to reducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import {render} from 'react-dom';
 
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import ReduxPromise from 'redux-promise';
 
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
 import App from './components/App';
 
-const store = applyMiddleware()(createStore);
+const store = applyMiddleware(ReduxPromise)(createStore);
 
 render(
 	<Provider store={store(reducers)}>
